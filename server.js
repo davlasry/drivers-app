3da3c507c65b2fe27486ab1e2af0abc0788db0da
Lasry David <lasry.david@gmail.com>
2019-08-09 03:46:23 +0300

    change nodejs main route

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.static('./dist/herolo-exercise'));
 
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, '/dist/drivers-app/index.html'));
+  res.sendFile(path.join(__dirname, '/dist/drivers-map/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
