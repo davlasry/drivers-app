991e9caa7209f48968e9b53bb38b015568bb1c12
Lasry David <lasry.david@gmail.com>
2019-08-10 23:22:04 +0300

    use tap instead of subscribe in driver-list compon

--- src/app/components/drivers-list/drivers-list.component.ts
+++ src/app/components/drivers-list/drivers-list.component.ts
@@ -3,13 +3,14 @@ import { DriversService } from 'src/app/services/drivers.service';
 import { Observable, Subscription } from 'rxjs';
 import { GoogleMapService } from 'src/app/services/google-map.service';
 import { IDriver } from 'src/app/interfaces/driver';
+import { tap, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-drivers-list',
   templateUrl: './drivers-list.component.html',
   styleUrls: ['./drivers-list.component.scss']
 })
-export class DriversListComponent implements OnInit, OnDestroy {
+export class DriversListComponent implements OnInit {
   drivers$: Observable<IDriver[]>;
   selectedDriverId: string;
   driversSubscription: Subscription;
@@ -21,24 +22,15 @@ export class DriversListComponent implements OnInit, OnDestroy {
   ) {}
 
   ngOnInit() {
-    this.isAlreadyLoaded = true;
-
     // Get drivers list
-    this.drivers$ = this.driversService.drivers$;
-
-    // Subscribe to the drivers Observable
-    this.driversSubscription = this.driversService.drivers$.subscribe(
-      drivers => {
-        // Set the first driver as the default one
+    this.drivers$ = this.driversService.drivers$.pipe(
+      tap(drivers => {
         if (drivers.length > 0 && !this.isAlreadyLoaded) {
           this.setDriverLocation(drivers[0]);
+          this.isAlreadyLoaded = true;
         }
-      }
+      })
     );
-  }
-
-  ngOnDestroy() {
-    this.driversSubscription.unsubscribe();
   }
 
   setDriverLocation(driver: IDriver): void {
